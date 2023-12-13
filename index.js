af0987dc94835e3fac32028e30ae662efef98801
Fabien SGARD <fsg@newsteo.com>
2023-12-13 11:31:21 +0100

    Renaming

--- index.js
+++ index.js
@@ -3,16 +3,15 @@ import { TypeJSErrorMode, TypeJSError, ArgumentsControllerError, InterfaceError,
 import Class from "./lib/Class";
 import { Enum, MetaType, T, TypeJSType } from "./lib/Types";
 
-export default class TypeJS {
+export default class ScriptTypeJS {
     static mode = 'dev';
     static errorMode = TypeJSErrorMode.default;
     static interfaces = {};
     static types = {};
-    static prefix = '__type_js__';
+    static prefix = '__SCRIPT_TYPE_JS__';
     static usePrivateSynthax = false;
-    static methodPropertyName = TypeJS.prefix + '_methods';
-    static instanciationPropertyName = TypeJS.prefix + '_instantiate';
-    static globalStatePropertyName = TypeJS.prefix + 'state';
+    static methodPropertyName = ScriptTypeJS.prefix + 'METHODS';
+    static instanciationPropertyName = ScriptTypeJS.prefix + 'INSTANCIATE';
     static bundlerIdentifier = {
         WEBPACK: {
             regex: /(_(.*)__WEBPACK.*__)(\.((.*)(\[.*\])|.*)|.*)/,
@@ -33,62 +32,65 @@ export default class TypeJS {
 
     static settings(options) {
         if (options) {
-            TypeJS.mode = options.mode ?? 'dev';
-            TypeJS.errorMode = options.errorMode ?? '';
-            TypeJSError.mode = TypeJS.errorMode;
-            TypeJS.prefix = options.prefix ?? TypeJS.prefix;
+            ScriptTypeJS.mode = options.mode ?? 'dev';
+            ScriptTypeJS.errorMode = options.errorMode ?? '';
+            TypeJSError.mode = ScriptTypeJS.errorMode;
+            ScriptTypeJS.prefix = options.prefix ?? ScriptTypeJS.prefix;
         }
 
-        if (TypeJS.mode == 'dev') {
-            Object.entries(TypeJS.types).forEach(([name, declaration]) => {
+        if (ScriptTypeJS.mode == 'dev') {
+            Object.entries(ScriptTypeJS.types).forEach(([name, declaration]) => {
                 if (declaration instanceof TypeJSType) declaration.declare(name);
                 else Class.construct(declaration);
             });
         }
-        return TypeJS;
+        return ScriptTypeJS;
     }
 
     static declare(objects) {
         if (typeof objects === 'function') {
-            TypeJS.types[objects.name] = objects;
+            ScriptTypeJS.types[objects.name] = objects;
             return new Class(objects);
         }
 
         Object.entries(objects).forEach(([name, obj]) => {
-            TypeJS.types[name] = obj;
+            ScriptTypeJS.types[name] = obj;
         });
 
     }
 
     static getTypeJSPrefix() {
-        return `${TypeJS.usePrivateSynthax ? '#' : ''}${TypeJS.prefix}`;
+        return `${ScriptTypeJS.usePrivateSynthax ? '#' : ''}${ScriptTypeJS.prefix}`;
     }
 
     static getTypeJSNameOf(value) {
-        return TypeJS.getTypeJSPrefix() + value;
+        return ScriptTypeJS.getTypeJSPrefix() + value;
     }
 
     static getTypeDeclaration(type) {
         if (!type) return undefined;
         ArgumentsController.declare(String).validate(arguments);
 
-        for (const [bundler, description] of Object.entries(TypeJS.bundlerIdentifier)) {
+        for (const [bundler, description] of Object.entries(ScriptTypeJS.bundlerIdentifier)) {
             const result = description.test(type)
-            if (result) type = result.type
+            if (result) {
+                type = result.type;
+                break;
+            }
         }
 
-        if (TypeJS.types[type]) return `${TypeJS.prefix}.types.${type}`;
-        else if (TypeJS.interfaces[type]) return `${TypeJS.prefix}.interfaces.${type}`;
+        if (ScriptTypeJS.types[type]) return `${ScriptTypeJS.prefix}.types.${type}`;
+        else if (ScriptTypeJS.interfaces[type]) return `${ScriptTypeJS.prefix}.interfaces.${type}`;
         else {
             const isObjectStaticProperty = type.match(/^(.*)\["(.*)"\]$/)
             if (isObjectStaticProperty) {
-                if (TypeJS.types[isObjectStaticProperty[1]]) return `${TypeJS.prefix}.types.${type}`;
+                if (ScriptTypeJS.types[isObjectStaticProperty[1]]) return `${ScriptTypeJS.prefix}.types.${type}`;
             }
             return type
         }
     }
 }
 
-global[TypeJS.prefix] = TypeJS;
+global[ScriptTypeJS.prefix] = ScriptTypeJS;
 
 export { TypeJSErrorMode, TypeJSError, ArgumentsController, ArgumentsControllerError, Class, ClassError, InterfaceError, Enum, MetaType, T };
